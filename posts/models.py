c43c866505cc505143527b883a5812519a668a90
KingVulkan <manowfelow@gmail.com>
2019-03-18 08:02:46 +0300

    adds profile class in models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to ='prof_pictures/')
+    bio = HTMLField()
+    phone = models.IntegerField(default=0) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def get_profile(cls,id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls,id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
