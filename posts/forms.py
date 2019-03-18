4f5eff821da700b5b913cee4a9c6786d161e655d
KingVulkan <manowfelow@gmail.com>
2019-03-18 09:48:37 +0300

    finishes forms

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Project, Reviews
 
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text = 'Required')
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
