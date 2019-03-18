97f3c9918e2a5bcccea17ba2d4518604757b8550
KingVulkan <manowfelow@gmail.com>
2019-03-18 09:27:53 +0300

    creates a form for projects

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Project, Reviews
 
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+       
+        exclude = ['user_profile']
+
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
